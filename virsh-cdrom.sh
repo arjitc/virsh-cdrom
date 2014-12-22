829fe092d3958ee82d5b755df4c4b71aa70dfe78
Arjit Chaudhary <arjit.c@gmail.com>
2014-12-22 17:42:45 +0530

    0.001
    The first working build with working logic in it.

--- virsh-cdrom.sh
+++ virsh-cdrom.sh
@@ -1,18 +1,43 @@
 #!/bin/bash
-## The folder under which your KVM configuration files exist
+## The folder under which your KVM configuration files exist, no trailing slash
 CONFIG_LOCATION="/etc/libvirt/qemu"
-## The folder under which your ISO files exist
-ISO_LOCATION="/var/lib/libvirt/images/"
+## The folder under which your ISO files exist, no trailing slash
+ISO_LOCATION="/var/lib/libvirt/images"
 
-action=$1
-if [[ -z "$action" ]]
+ACTION=$1
+GUEST=$2
+ISO=$3
+
+if [[ -z "$ACTION" ]]
 then
-   echo "Action - unmount or mount is missing"
+   echo "Error: unmount or mount is undefined \n"
+   echo "Example: \n"
+   echo "Mount: sh virsh-cdrom.sh mount GUEST_NAME ISO_NAME \n"
+   echo "Unmount: sh virsh-cdrom.sh unmount GUEST_NAME ISO_NAME"
    exit
 fi
-if [ "$action" == mount ]; then
-	echo "Mount!"
+if [ "$ACTION" == mount ]; then
+	# shutdown the guest
+	/usr/bin/virsh shutdown $GUEST
+	# unmount any old ISO's
+	/usr/bin/virsh change-media $GUEST hdc --eject
+	# mount new ISO
+	/usr/bin/virsh change-media $GUEST hdc $ISO_LOCATION/$ISO --insert
+	# change boot order
+	cat $CONFIG_LOCATION/$GUEST.xml | sed -e 's/hd/cdrom/g'
+	/usr/bin/virsh define $CONFIG_LOCATION/$GUEST.xml
+	# boot guest
+	/usr/bin/virsh create $CONFIG_LOCATION/$GUEST.xml
+	
 fi
-if [ "$action" == unmount ]; then
-	echo "unount!"
+if [ "$ACTION" == unmount ]; then
+	# shutdown the guest
+	/usr/bin/virsh shutdown $GUEST
+	# unmount any old ISO's
+	/usr/bin/virsh change-media $GUEST hdc --eject
+	# change boot order
+	cat $CONFIG_LOCATION/$GUEST.xml | sed -e 's/cdrom/hd/g'
+	/usr/bin/virsh define $CONFIG_LOCATION/$GUEST.xml
+	# boot guest
+	/usr/bin/virsh create $CONFIG_LOCATION/$GUEST.xml
 fi

a052c01ba9b888956249f83d7ba64110c904b967
Arjit Chaudhary <arjit.c@gmail.com>
2015-06-30 20:13:16 +0530

    Added error for missing ISO name

--- virsh-cdrom.sh
+++ virsh-cdrom.sh
@@ -24,6 +24,14 @@ then
    echo "Unmount: sh virsh-cdrom.sh unmount GUEST_NAME"
    exit
 fi
+if [[ -z "$ISO" ]]
+then
+   echo "Error: ISO mount name undefined"
+   echo "Example:"
+   echo "Mount: sh virsh-cdrom.sh mount GUEST_NAME ISO_NAME"
+   echo "Unmount: sh virsh-cdrom.sh unmount GUEST_NAME"
+   exit
+fi
 if [ "$ACTION" == mount ]; then
 	# shutdown the guest
 	/usr/bin/virsh destroy $GUEST
